34f06aad40696f522ab1981b6c4242bbd2c37194
OedoSoldier <wangsihan1995@gmail.com>
2024-10-29 08:56:46 +0800

    .

--- pretrain.sh
+++ pretrain.sh
@@ -29,7 +29,7 @@ deepspeed --include localhost:5 --master_port 2340 llava/train/train_mem.py \
     --num_train_epochs 1 \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 16 \
+    --gradient_accumulation_steps 32 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 24000 \
